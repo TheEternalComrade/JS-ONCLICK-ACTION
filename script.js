daaaa63f6fcee07290b556cc47618f32aa3ad52f
TheEternalComrade <taconora52@gmail.com>
2025-04-16 03:28:13 +0000

    new file:   index.html 	new file:   script.js 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,10 @@
+function doSomething(){
+    document.getElementById("secretMessage").setAttribute("class", "")//gets the html with the id of secretMessage and sets the
+    //the class attribute to nothing, meaning it no longer has a class of "hidden"
+}
+
+//something more interesting if we have time
+/**function roll(){
+    ranNum = Math.random()* 10
+    document.getElementById("numDisplay").innerHTML = ranNum
+}**/
